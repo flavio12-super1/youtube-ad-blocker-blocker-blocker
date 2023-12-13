0d7756cc93e769e6ae06b2258dd0ed901481c941
hacker-flavio <theflavioherrera@gmail.com>
2023-12-12 22:00:31 -0800

    udpate to ad blocker

--- init.js
+++ init.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     const videoId = myUrl.slice(32, 43);
 
     let intervalId = setInterval(function () {
-      const body = document.body;
+      const body = document.getElementById("player-container-outer");
 
       if (body) {
         const videos = body.querySelectorAll("video");
@@ -19,15 +19,107 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     setTimeout(function () {
       clearInterval(intervalId);
     }, 1000 * 60);
+
+    // alert("myurl: " + myUrl);
+
     if (myUrl.slice(0, 24) == "https://www.youtube.com/") {
-      if (myUrl.length > 24) {
+      document.body.style.overflow = "scroll";
+
+      // if (document.getElementById("miniOverlay")) {
+      //   console.log("miniOverlay already exists");
+      //   if (document.getElementById("myOverlayVideo")) {
+      //     newDiv.appendChild(document.getElementById("myOverlayVideo"));
+      //   }
+      // } else {
+      //   var newDiv = document.createElement("div");
+      //   newDiv.className = "miniOverlay";
+      //   document.documentElement.appendChild(newDiv);
+
+      //   if (document.getElementById("myOverlayVideo")) {
+      //     newDiv.appendChild(document.getElementById("myOverlayVideo"));
+      //   }
+      // }
+
+      // document.getElementById("primary-inner").style.display = "flex";
+      // document.getElementById("primary-inner").style.flexDirection = "column";
+      // document.getElementById("primary-inner").style.height =
+      //   "calc(100vh - 100px)";
+
+      // document.getElementById("secondary-inner").style.height = "calc(100vh - 100px)";
+      // document.getElementById("secondary-inner").style.overflow = "scroll";
+
+      // document.getElementById("below").style.overflow = "scroll";
+      // document.getElementById("below").style.flexGrow = "1";
+
+      if (myUrl.slice(0, 25) == "https://www.youtube.com/@") {
+        // alert("this is not a video");
         const myOverlayVideo = document.getElementById("myOverlayVideo");
-        myOverlayVideo.className = "myOverlayVideo";
-        myOverlayVideo.innerHTML = `  <iframe width="100%" height="720" id="myIframe" src="https://www.youtube.com/embed/${videoId}?autoplay=1" title="World Of Hardstyle 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        if (myOverlayVideo) {
+          myOverlayVideo.className = "myOverlayMiniVideo";
+          document.getElementById("myIframe").style.height = "300px";
+        }
       } else {
-        const myOverlayVideo = document.getElementById("myOverlayVideo");
-        myOverlayVideo.className = "myOverlayMiniVideo";
-        document.getElementById("myIframe").style.height = "300px";
+        // alert("found video");
+        if (
+          (myUrl.length > 24 && myUrl.slice(24, 31) != "results") ||
+          myUrl.slice(24, 31) != "channel"
+        ) {
+          document.body.style.overflow = "hidden";
+
+          document.getElementById("primary-inner").style.display = "flex";
+          document.getElementById("primary-inner").style.flexDirection =
+            "column";
+          document.getElementById("primary-inner").style.height =
+            "calc(100vh - 100px)";
+
+          document.getElementById("secondary-inner").style.height =
+            "calc(100vh - 100px)";
+          document.getElementById("secondary-inner").style.overflow = "scroll";
+
+          document.getElementById("below").style.overflow = "scroll";
+          document.getElementById("below").style.flexGrow = "1";
+          const myOverlayVideo = document.getElementById("myOverlayVideo");
+          if (myOverlayVideo) {
+            myOverlayVideo.className = "myOverlayVideo";
+            myOverlayVideo.innerHTML = `  <div class="pain">  <iframe width="100%" height="100%" id="myIframe" src="https://www.youtube.com/embed/${videoId}?autoplay=1" title="World Of Hardstyle 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>`;
+          } else {
+            // alert("adding vidoe video overlay");
+            if (document.getElementById("myOverlayVideo")) {
+              console.log("myOverlayVideo already exists");
+            } else {
+              const myOverlayVideo = document.createElement("div");
+              myOverlayVideo.className = "myOverlayVideo";
+              myOverlayVideo.id = "myOverlayVideo";
+              if (
+                myUrl.length > 24 &&
+                (myUrl.slice(24, 31) != "results" ||
+                  myUrl.slice(24, 31) != "channel")
+              ) {
+                myOverlayVideo.innerHTML = ` <div class="pain"> <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}?autoplay=1" title="World Of Hardstyle 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> </div>`;
+              }
+              // document.documentElement.appendChild(myOverlayVideo);
+              //document.getElementById("player").appendChild(myOverlayVideo);
+              // Create a new div element
+              // var newDiv = document.createElement("div");
+              // newDiv.textContent = "This is the new div!"; // You can customize the content as needed
+
+              // // Get the parent node
+              var parentContainer = document.getElementById(
+                "player-container-outer"
+              );
+
+              // Insert the new div before the existing child (assuming there is a child)
+              parentContainer.insertBefore(
+                myOverlayVideo,
+                parentContainer.firstChild
+              );
+            }
+          }
+        } else {
+          const myOverlayVideo = document.getElementById("myOverlayVideo");
+          myOverlayVideo.className = "myOverlayMiniVideo";
+          document.getElementById("myIframe").style.height = "300px";
+        }
       }
     }
   }
@@ -35,11 +127,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 document.addEventListener("DOMContentLoaded", function () {
   var currentUrl = window.location.href;
-  console.log(currentUrl);
+  console.log(currentUrl, "currentUrl");
+  // alert("currentUrl: " + currentUrl);
   const videoId = currentUrl.slice(32, 43);
+  //   const resultText = currentUrl.slice(24, 31);
 
   let intervalId = setInterval(function () {
-    const body = document.body;
+    const body = document.getElementById("player-container-outer");
 
     if (body) {
       const videos = body.querySelectorAll("video");
@@ -55,12 +149,82 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 1000 * 60);
   // if (currentUrl.length > 24) {
   if (currentUrl.slice(0, 24) == "https://www.youtube.com/") {
-    const myOverlayVideo = document.createElement("div");
-    myOverlayVideo.className = "myOverlayVideo";
-    myOverlayVideo.id = "myOverlayVideo";
-    if (currentUrl.length > 24) {
-      myOverlayVideo.innerHTML = `  <iframe width="100%" height="720" src="https://www.youtube.com/embed/${videoId}?autoplay=1" title="World Of Hardstyle 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+    document.body.style.overflow = "scroll";
+
+    // if (document.getElementById("miniOverlay")) {
+    //   console.log("miniOverlay already exists");
+
+    //   if (document.getElementById("myOverlayVideo")) {
+    //     newDiv.appendChild(document.getElementById("myOverlayVideo"));
+    //   }
+    // } else {
+    //   var newDiv = document.createElement("div");
+    //   newDiv.className = "miniOverlay";
+    //   document.documentElement.appendChild(newDiv);
+
+    //   if (document.getElementById("myOverlayVideo")) {
+    //     newDiv.appendChild(document.getElementById("myOverlayVideo"));
+    //   }
+    // }
+
+    // document.getElementById("primary-inner").style.display = "flex";
+    // document.getElementById("primary-inner").style.flexDirection = "column";
+    // document.getElementById("primary-inner").style.height =
+    //   "calc(100vh - 100px)";
+
+    // document.getElementById("secondary-inner").style.height = "calc(100vh - 100px)";
+    // document.getElementById("secondary-inner").style.overflow = "scroll";
+    // document.getElementById("below").style.overflow = "scroll";
+    // document.getElementById("below").style.flexGrow = "1";
+    // document.getElementById("player").style.height = "auto";
+
+    //alert("currentUrl second alert: " + currentUrl);
+    if (currentUrl.slice(0, 25) == "https://www.youtube.com/@") {
+      //  alert("this is not a video");
+      console.log("this is not a video");
+    } else if (currentUrl.length > 24) {
+      document.body.style.overflow = "hidden";
+
+      document.getElementById("primary-inner").style.display = "flex";
+      document.getElementById("primary-inner").style.flexDirection = "column";
+      document.getElementById("primary-inner").style.height =
+        "calc(100vh - 100px)";
+
+      document.getElementById("secondary-inner").style.height =
+        "calc(100vh - 100px)";
+      document.getElementById("secondary-inner").style.overflow = "scroll";
+
+      document.getElementById("below").style.overflow = "scroll";
+      document.getElementById("below").style.flexGrow = "1";
+      if (document.getElementById("myOverlayVideo")) {
+        console.log("myOverlayVideo already exists");
+      } else {
+        // alert("adding vidoe video overlay");
+        const myOverlayVideo = document.createElement("div");
+        myOverlayVideo.className = "myOverlayVideo";
+        myOverlayVideo.id = "myOverlayVideo";
+        if (
+          currentUrl.length > 24 &&
+          (currentUrl.slice(24, 31) != "results" ||
+            currentUrl.slice(24, 31) != "channel")
+        ) {
+          myOverlayVideo.innerHTML = `  <div class="pain"><iframe width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}?autoplay=1" title="World Of Hardstyle 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>`;
+        }
+        // document.documentElement.appendChild(myOverlayVideo);
+        // Create a new div element
+        // var newDiv = document.createElement("div");
+        // newDiv.textContent = "This is the new div!"; // You can customize the content as needed
+
+        // Get the parent node
+        var parentContainer = document.getElementById("player-container-outer");
+
+        // Insert the new div before the existing child (assuming there is a child)
+
+        parentContainer.insertBefore(
+          myOverlayVideo,
+          parentContainer.firstChild
+        );
+      }
     }
-    document.documentElement.appendChild(myOverlayVideo);
   }
 });
